6e76dab903eacc8b94bb137be26e0c9ff2788c2b
Evgeny Chuvelev <e.chuvelev@banki.ru>
2015-06-18 21:02:28 +0300

    Connect to MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../helpers/mongo_connection.js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  connection.db.collection('users').find().toArray(function(err, result) {
+    if (err) throw err;
+    res.render('users', {
+      title: 'Список водителей',
+      users : result
+    });
+    console.log(result);
+  });
+
+});
+
+router.get('/add', function(req, res, next) {
+  res.render('users/add', {
+    title: 'Добавить водителя'
+  });
 });
 
 module.exports = router;
